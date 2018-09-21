23560424ed3fc0880ac4757c615fcf9c8e8355be
ovidiudrumia <ovidiu.drumia@yahoo.com>
2018-09-21 13:26:06 +0300

    Deadpool vs Gone with the wind

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MovieService } from './shared/movie/movie.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [HttpClientModule],
+      providers: [MovieService, HttpClient]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
--- client/src/app/shared/movie/movie.service.spec.ts
+++ client/src/app/shared/movie/movie.service.spec.ts
@@ -4,7 +4,6 @@ import { MovieService } from './movie.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { MovieService } from './shared/movie/movie.service';
 import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
